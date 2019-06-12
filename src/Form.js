15de397a4ef6be6bed44cd9f31d5ce20074a08f7
Dale King <dale@studiomatrix.com.au>
2019-06-12 18:15:10 +1000

    Handling dates and validation

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,9 @@ import { Formik } from 'formik'
 import * as yup from 'yup'
 
 import { Form, Input, Datepicker, SubmitBtn } from 'react-formik-ui'
+import { addDays } from 'date-fns'
+
+const nextWeek = addDays(new Date(), 7)
 
 class MainForm extends Component {
   onSubmit = data => {
@@ -12,8 +15,20 @@ class MainForm extends Component {
 
   // example of validation with yup
   getSchema = () => {
+    console.log('Called')
     return yup.object().shape({
-      birthday: yup.date(),
+      clientName: yup
+        .string()
+        .min(2, "the client's name is longer than that...")
+        .required('Required'),
+      projectName: yup
+        .string()
+        .min(2, 'Lets give the project name a bit more length...')
+        .required('Required'),
+      dueDate: yup
+        .date()
+        .min(nextWeek, "You can't be serious")
+        .required('Please set a due date'),
     })
   }
 
@@ -21,7 +36,7 @@ class MainForm extends Component {
     return (
       <Formik
         initialValues={{
-          birthday: '',
+          dueDate: '',
           clientName: '',
           projectName: '',
         }}
@@ -38,16 +53,20 @@ class MainForm extends Component {
                   : 'Lets Get Started'}
               </div>
 
+              {/* Client Name */}
               <Input
                 name="clientName"
                 placeholder="Client Name"
                 label="Client Name"
+                required
+                autocomplete="off"
               />
 
               <Input
                 name="projectName"
                 placeholder="Project Name"
                 label="Project Name"
+                autocomplete="off"
               />
 
               <Datepicker name="dueDate" label="Due Date" />
